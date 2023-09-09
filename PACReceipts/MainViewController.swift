002a7094d954b47dcc090908e61141aa9a5485d7
kent-daniel <kentdaniel0328@gmail.com>
2023-09-09 20:05:55 +1000

    partial homescreen + tabbar

--- PACReceipts/MainViewController.swift
+++ PACReceipts/MainViewController.swift
@@ -8,42 +8,125 @@
 import UIKit
 
 class MainViewController: UIViewController {
-    let titleView = UIView()
-    let titleLabel = UILabel()
+    enum SectionLayoutKind: Int, CaseIterable {
+        case categories, monthCell
+        var columnCount: Int {
+            switch self {
+            case .monthCell:
+                return 1
+            case .categories:
+                return 3
+            }
+        }
+    }
+    
+    var categories: [Category]?
+    private var dataSource: UICollectionViewDiffableDataSource<SectionLayoutKind, Int>! = nil
+    private var collectionView: UICollectionView! = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configViews()
+        configureHierarchy()
+        configureDataSource()
     }
     
     func configViews(){
-        titleView.translatesAutoresizingMaskIntoConstraints = false
-        titleView.backgroundColor = UIColor.lightGray
-        titleView.layer.cornerRadius = 20
-        view.addSubview(titleView)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(addButtonTapped))
         
-        NSLayoutConstraint.activate([
-            titleView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            titleView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            titleView.widthAnchor.constraint(equalToConstant: 200),  // Adjust the width as needed
-            titleView.heightAnchor.constraint(equalToConstant: 100)  // Adjust the height as needed
-            
-        ])
-        
-        
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.text = "Hello, Receipts!" // Set the text you want to display
-        titleLabel.textColor = UIColor.white
-        titleLabel.textAlignment = .center
-        titleView.addSubview(titleLabel)
-        
-        // Center the label in the titleView.
-        NSLayoutConstraint.activate([
-            titleLabel.centerXAnchor.constraint(equalTo: titleView.centerXAnchor),
-            titleLabel.centerYAnchor.constraint(equalTo: titleView.centerYAnchor)
-        ])
     }
     
-    
+    @objc
+    func addButtonTapped() {
+        
+    }
+}
+
+extension MainViewController {
+    /// - Tag: PerSection
+    func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int,
+            layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+
+            guard let sectionLayoutKind = SectionLayoutKind(rawValue: sectionIndex) else { return nil }
+            let columns = sectionLayoutKind.columnCount
+
+            // The group auto-calculates the actual item width to make
+            // the requested number of columns fit, so this widthDimension is ignored.
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                 heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+
+            let groupHeight = NSCollectionLayoutDimension.absolute(44)
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                  heightDimension: groupHeight)
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: columns)
+
+            let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .continuous
+            section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+            return section
+        }
+        return layout
+    }
+}
+
+extension MainViewController {
+    func configureHierarchy() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+    }
+    func configureDataSource() {
+        
+        let listCellRegistration = UICollectionView.CellRegistration<TextCell, String> { (cell, indexPath, identifier) in
+            // Populate the cell with our item description.
+            cell.label.text = "\(identifier)"
+            cell.contentView.layer.borderColor = UIColor.gray.cgColor
+            cell.contentView.layer.borderWidth = 1
+            cell.contentView.layer.cornerRadius = 20
+            cell.label.textAlignment = .center
+            cell.label.font = UIFont.preferredFont(forTextStyle: .body)
+        }
+        
+        let textCellRegistration = UICollectionView.CellRegistration<TextCell, Int> { (cell, indexPath, identifier) in
+            // Populate the cell with our item description.
+            cell.label.text = "\(identifier)"
+            cell.contentView.backgroundColor = .systemBlue
+            cell.contentView.layer.borderColor = UIColor.black.cgColor
+            cell.contentView.layer.borderWidth = 1
+            cell.contentView.layer.cornerRadius = 8
+            cell.label.textAlignment = .center
+            cell.label.font = UIFont.preferredFont(forTextStyle: .title1)
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource<SectionLayoutKind, Int>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
+            // Return the cell.
+            return SectionLayoutKind(rawValue: indexPath.section)! == .categories ?
+            collectionView.dequeueConfiguredReusableCell(using: listCellRegistration, for: indexPath, item: "Category" ) :
+            collectionView.dequeueConfiguredReusableCell(using: textCellRegistration, for: indexPath, item: identifier)
+        }
+
+        // initial data
+        let itemsPerSection = 10
+        var snapshot = NSDiffableDataSourceSnapshot<SectionLayoutKind, Int>()
+        SectionLayoutKind.allCases.forEach {
+            snapshot.appendSections([$0])
+            let itemOffset = $0.rawValue * itemsPerSection
+            let itemUpperbound = itemOffset + itemsPerSection
+            snapshot.appendItems(Array(itemOffset..<itemUpperbound))
+        }
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+}
+
+extension MainViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
 }
 
---
+++ PACReceipts/Views/TextCell.swift
@@ -0,0 +1,42 @@
+//
+//  TextCell.swift
+//  PACReceipts
+//
+//  Created by kent daniel on 9/9/2023.
+//
+
+import Foundation
+
+
+
+import UIKit
+
+class TextCell: UICollectionViewCell {
+    let label = UILabel()
+    static let reuseIdentifier = "text-cell-reuse-identifier"
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("not implemnted")
+    }
+
+}
+
+extension TextCell {
+    func configure() {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = true
+        contentView.addSubview(label)
+        label.font = UIFont.preferredFont(forTextStyle: .caption1)
+        let inset = CGFloat(10)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset),
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -inset),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: inset),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -inset)
+            ])
+    }
+}
